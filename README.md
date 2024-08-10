83ed59f4e7d5ecbf0e3eaccb3f22e75c04436a1f
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2024-08-10 14:00:04 +0800

    feat(nix): :sparkles: add nix build script

--- README.md
+++ README.md
@@ -4,4 +4,36 @@ A taste of Spring Boot and Kotlin.
 
 JDK 21 and GraalVM are supported. Uses Version Catalog.
 
-Also provides a `flake.nix` for Nix users. Use `nix develop` to enter the environment.
+Also provides a `flake.nix` for Nix users.
+
+## Nix
+
+### Developing
+
+After updating gradle dependencies, run:
+
+```bash
+nix run .#update-deps
+```
+
+Or in nix develop environment:
+
+```bash
+nix develop
+# in develop shell
+update-deps
+```
+
+Then in nix develop environment:
+
+```bash
+./gradlew bootRun
+```
+
+### Building
+
+```bash
+nix build
+```
+
+Then use `./result/bin/SpringPlay` to start the server.
--- flake.nix
+++ flake.nix
@@ -1,25 +1,81 @@
 {
-  inputs = { };
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+  };
   outputs =
-    { nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in
-    # deno = pkgs.callPackage ./deno.nix { };
-    {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          (graalvmCEPackages.buildGraalvm {
-            useMusl = false;
-            src = fetchurl {
-              url = "https://github.com/graalvm/graalvm-ce-builds/releases/download/jdk-21.0.2/graalvm-community-jdk-21.0.2_linux-x64_bin.tar.gz";
-              hash = "sha256-sEgGmqo6mbhPW5V7FizBgaMqQzDLw1QCdmNjxb52rkg=";
-            };
-            version = "21.0.2";
-            meta.platforms = [ system ];
-          })
-        ];
-      };
-    };
+    { nixpkgs, utils, ... }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (self: super: { gradle = super.gradle.override { java = graalVM; }; }) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        graalVMUrls = {
+          x86_64-linux = {
+            url = "https://github.com/graalvm/graalvm-ce-builds/releases/download/jdk-21.0.2/graalvm-community-jdk-21.0.2_linux-x64_bin.tar.gz";
+            hash = "sha256-sEgGmqo6mbhPW5V7FizBgaMqQzDLw1QCdmNjxb52rkg=";
+          };
+          aarch64-darwin = {
+            url = "https://github.com/graalvm/graalvm-ce-builds/releases/download/jdk-21.0.2/graalvm-community-jdk-21.0.2_macos-aarch64_bin.tar.gz";
+            hash = "sha256-UV46k6zH4ZONq6g+2kJy5Ulf0wLXzdmex+v0CO1QWrc=";
+          };
+        };
+        graalVM = pkgs.graalvmCEPackages.buildGraalvm {
+          useMusl = false;
+          src = pkgs.fetchurl graalVMUrls.${system};
+          version = "21.0.2";
+          meta.platforms = [ system ];
+        };
+      in
+      # deno = pkgs.callPackage ./deno.nix { };
+      rec {
+        fetchd = pkgs.gradle;
+        mitmC = pkgs.mitm-cache;
+        packages.default =
+          let
+            self = pkgs.stdenv.mkDerivation (finalAttrs: {
+              pname = "spring-play";
+              version = "1.0.0";
+              src = ./.;
+              nativeBuildInputs = with pkgs; [ gradle ];
+              buildInputs = with pkgs; [ makeWrapper ];
+              mitmCache = pkgs.gradle.fetchDeps {
+                inherit (finalAttrs) pname;
+                pkg = self;
+                data = ./deps.json;
+              };
+              __darwinAllowLocalNetworking = true;
+              gradleUpdateTask = "bootJar";
+              gradleBuildTask = "bootJar";
+              installPhase = ''
+                runHook preInstall
+                mkdir -p $out/{bin,share/SpringPlay}
+                cp build/libs/SpringPlay-1.0-SNAPSHOT.jar $out/share/SpringPlay
+                makeWrapper ${graalVM}/bin/java $out/bin/SpringPlay \
+                  --add-flags "-jar $out/share/SpringPlay/SpringPlay-1.0-SNAPSHOT.jar"
+                runHook postInstall
+              '';
+            });
+          in
+          self;
+        packages.updateDeps = pkgs.stdenv.mkDerivation {
+          # src = packages.default.mitmCache.updateScript;
+          pname = "update-deps";
+          version = "0.0.1";
+          dontUnpack = true;
+          installPhase = ''
+            runHook preInstall
+            mkdir -p $out/bin
+            cp ${packages.default.mitmCache.updateScript} $out/bin/update-deps
+            runHook postInstall
+          '';
+        };
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            graalVM
+            gradle
+            packages.updateDeps
+          ];
+        };
+      }
+    );
 }
